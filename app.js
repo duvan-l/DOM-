a8acaf27538b0c55c43c6520d0e87f39a228caff
Duvan Leon <leonduvan11@gmail.com>
2024-06-13 11:41:47 -0500

    Repaso

---
+++ app.js
@@ -0,0 +1,10 @@
+//objeto padre en e lnavegador
+let win = window;
+let dom = document;
+let head = dom.head
+let body = dom.body
+
+console.log(win)
+console.log(dom)
+console.log(head)
+console.log(body)
